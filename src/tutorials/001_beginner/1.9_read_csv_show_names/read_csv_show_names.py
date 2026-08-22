4aae26457219117a4cba3c8d4abdec686c8255fb
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-08-22 10:19:24 -0400

    update

---
+++ src/tutorials/001_beginner/1.9_read_csv_show_names/read_csv_show_names.py
@@ -0,0 +1,26 @@
+# read_csv_show_names.py
+
+import pandas as pd
+
+theData = pd.read_csv('data.csv')
+
+print(theData["Name"])
+
+####
+
+'''
+0  Tabitha
+1  Jane
+2  Jennifer
+3  Alison, Martin
+Name: Name, dtype: object
+'''
+
+####
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2026
+# https://github.com/ChristopherAndrewTopalian
+# https://github.com/ChristopherTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
